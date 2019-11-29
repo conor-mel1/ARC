77d274b75ba75631c0e941a0fc2f5ef23e8af8a0
conor-mel1 <conormelody123@yahoo.com>
2019-11-29 08:04:54 +0000

    My test program

---
+++ src/test2.py
@@ -0,0 +1,21 @@
+'''
+Created on 22 Nov 2019
+
+@author: conor
+'''
+
+#Function 1
+y=[6,7,8,9,10]
+
+for i in y:
+    print(i)
+    
+    
+#Function 2
+
+def f(x):
+    return x*2 +1
+
+print("f(2) = ", f(2))
+    
+    
