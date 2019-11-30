b9cd5227b7b5e21c7d64bdc2e75386df5b01f7f4
conor-mel1 <conormelody123@yahoo.com>
2019-11-30 13:33:10 +0000

    Added piece to obtain patterened part of element of output array

--- src/solution_bbc9ae5d.py
+++ src/solution_bbc9ae5d.py
@@ -15,13 +15,21 @@ for x in range(OutputGrid_ArrayLength):
 print("Output Grid", Output_GridArray)
 
 #This piece calculates the first n nonzero elements of an array
-array = [1,1,0,0,0,0]
+array = [1,1,1,0,0,0]
 initial_nonzero_elements = []
 for x in array:
     if x!=0:
         initial_nonzero_elements.append(x)
 print(initial_nonzero_elements)
 
+#Way in which the patterned piece of each element of the output array is calculated
+patterened_piece = []
+for i in range(len(array)):
+    if array[i]==0:
+        new_array_input = array[i] + array[i-1]
+        patterened_piece.append(new_array_input)
+
+print("Patterened_Array", patterened_piece )
     
     
 
