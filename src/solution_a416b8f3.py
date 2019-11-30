23363187eb3bb3bf6f88a29187231335a2d43dfb
conor-mel1 <conormelody123@yahoo.com>
2019-11-30 14:28:47 +0000

    Added "Solve" method. Solves for all input cases in task

--- src/solution_a416b8f3.py
+++ src/solution_a416b8f3.py
@@ -5,12 +5,18 @@ Created on 30 Nov 2019
 '''
 
 
-input = [[0,5,0],[5,5,2],[0,0,0]] #This is an example of a typical input
+def solve(input_Json):
+    output_array = [] #Defining the desired output
+    for grid_row in input_Json:
+        grid_row += grid_row #Adding each element on to itslef
+        output_array.append(grid_row) #Appending to the output_array 
+    print("Output Grid = ", output_array)
+    return(output_array)
 
-output = [[0,5,0,0,5,0],[5,5,2,5,5,2],[0,0,0,0,0,0]] #This is an example of a typical output
+#Training Data
+solve([[0,5,0],[5,5,2],[0,0,0]])
+solve([[3, 0, 0], [2, 3, 0], [2, 1, 8], [0, 1, 0]])
+solve([[5, 2, 3, 0], [2, 5, 3, 0], [5, 2, 8, 8], [0, 0, 6, 0]])
 
-output_array = [] #Defining the desired output
-for grid_row in input:
-    grid_row += grid_row #Adding each element on to itslef
-    output_array.append(grid_row) #Appending to the output_array 
-print("Correct Output Grid = ", output_array)
+#Testing Data
+solve([[4, 0, 0, 0], [4, 5, 0, 0], [0, 5, 6, 0], [6, 6, 1, 0], [0, 0, 0, 1]])
