4447f5693345dae21b1080a8793c9c644dc5743a
conor-mel1 <conormelody123@yahoo.com>
2019-11-30 15:15:23 +0000

    Created correct transformation pattern for inputs

--- src/solution_62c24649.py
+++ src/solution_62c24649.py
@@ -13,4 +13,11 @@ for x in range(OutputGrid_ArrayLength):
     Output_GridRow = []
     Output_GridArray.append(Output_GridRow)
     #print(Output_GridArray)
-print(Output_GridArray)
+#print(Output_GridArray)
+
+#This piece applies the transfomation pattern to the input_Json
+patterned_string = [] #Defining desired output grid
+for x in input_Json:
+    y = x + x[::-1] #Create new variable y, which adds the reverse of each element of input_Json to itself 
+    patterned_string.append(y) #Appending each y to patterned_string
+print(patterned_string)
