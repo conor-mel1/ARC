6fc1efcf093a060a54f5ea0dcb6a622f49b812ae
conor-mel1 <conormelody123@yahoo.com>
2019-12-01 22:58:00 +0000

    Added Comments

--- src/solution_a416b8f3.py
+++ src/solution_a416b8f3.py
@@ -7,30 +7,29 @@ import sys
 def solve(Input_Grid):
     Output_Grid = [] #Defining the desired output
     for grid_row in Input_Grid:
-        grid_row += grid_row #Adding each element on to itslef
+        grid_row += grid_row #Adding each element on to itself
         Output_Grid.append(grid_row) #Appending to the output_array 
     
     for s in Output_Grid:
-        print("{0}".format(" ".join(str(x) for x in s)))
+        print("{0}".format(" ".join(str(x) for x in s))) #Removing the commas from Output_Grid elements
     return(Output_Grid) #Output of solve is also a Python list of lists
 
 #Run the program by typing python solution_a416b8f3.py  \path to Json file
+#
 if len(sys.argv) != 2:
     print("usage:python solution_a416b8f3.py <input Json file>")
     sys.exit(0)
 else:
-    input_file = sys.argv[1]
+    input_file = sys.argv[1] #This is the path to the file from the command line
     print("input_file", input_file)
     
 with open(input_file) as json_file:
-    json_data = json.load(json_file)  
+    json_data = json.load(json_file) #Loads json file into a json data structure in python 
     
 for i in range(len(json_data['train'])):
-    print("Training Output Grid ")
-    solve(json_data['train'][i]['input'])#Specifying elements of the json file to apply solve method to
+    solve(json_data['train'][i]['input'])#Specifying train input elements of the json file to apply solve method to
     print('\n')
     
 for i in range(len(json_data['test'])):
-    print("Testing Output Grid ")
-    solve(json_data['test'][i]['input'])
+    solve(json_data['test'][i]['input'])#Specifying test input elements of the json file to apply solve method to
     print('\n')
